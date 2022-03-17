02b849c6647df7f11b5c5f70e8b1aafbc1a0e67e
thaihn2801 <hoangthai.solem2801@gmail.com>
2022-03-17 17:11:09 +0700

    update cart

--- src/main/java/com/example/ecommerceweb/model/CartItem.java
+++ src/main/java/com/example/ecommerceweb/model/CartItem.java
@@ -1,45 +1,21 @@
-//package com.example.ecommerceweb.model;
-//
-//import lombok.Data;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Data
-//public class CartItem {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "product_id")
-//    private Product product;
-//
-//    private int quantity;
-//
-//    public Product getProduct() {
-//        return product;
-//    }
-//
-//    public void setProduct(Product product) {
-//        this.product = product;
-//    }
-//
-//    public int getQuantity() {
-//        return quantity;
-//    }
-//
-//    public void setQuantity(int quantity) {
-//        this.quantity = quantity;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//
-//}
+package com.example.ecommerceweb.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int quantity;
+    private double total;
+
+
+}
