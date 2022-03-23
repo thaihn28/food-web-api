884903421d277eeb60121c1163581358178a65c1
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-24 01:28:51 +0700

    refactor folder structure

---
+++ src/main/java/com/example/ecommerceweb/api/product/ProductAPI.java
@@ -0,0 +1,50 @@
+package com.example.ecommerceweb.api.product;
+
+import com.example.ecommerceweb.model.Product;
+import com.example.ecommerceweb.service.ProductService;
+import com.example.ecommerceweb.service.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/products")
+@RestController
+public class ProductAPI {
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> getAllProducts(
+            @RequestParam(value = "order", required = false, defaultValue = "ASC") String order,
+            @RequestParam(value = "orderBy", required = false, defaultValue = "name") String orderBy,
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        try {
+            Page<Product> pageRes = productService.getProductsWithPaginationAndSorting(page - 1, pageSize, orderBy, order);
+            Map<String, Object> response = new HashMap<>();
+            response.put("data", pageRes.getContent());
+            response.put("currentPage", pageRes.getNumber());
+            response.put("totalItems", pageRes.getTotalElements());
+            response.put("totalPages", pageRes.getTotalPages());
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping("/{id}")
+    public Product productById(@PathVariable("id") Long id) {
+        try {
+            return productService.getProductById(id);
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/main/java/com/example/ecommerceweb/controller/HomeController.java
+++ src/main/java/com/example/ecommerceweb/controller/HomeController.java
@@ -23,42 +23,6 @@ public class HomeController {
     @Autowired
     CategoryService categoryService;
 
-    @GetMapping("/products")
-    public ResponseEntity<Map<String, Object>> getAllProducts(
-            @RequestParam(value = "order", required = false, defaultValue = "ASC") String order,
-            @RequestParam(value = "orderBy", required = false, defaultValue = "name") String orderBy,
-            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
-            @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize) {
-        try {
-            Page<Product> pageRes = productService.getProductsWithPaginationAndSorting(page - 1, pageSize, orderBy, order);
-            Map<String, Object> response = new HashMap<>();
-            response.put("data", pageRes.getContent());
-            response.put("currentPage", pageRes.getNumber());
-            response.put("totalItems", pageRes.getTotalElements());
-            response.put("totalPages", pageRes.getTotalPages());
-
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    // products
-    @GetMapping("/products/{id}")
-    public Product productById(@PathVariable("id") Long id){
-        try {
-            return productService.getProductById(id);
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    @GetMapping("/products/{name}")
-    public List<Product> getAllProductByName(@PathVariable("name") String name){
-        return productService.getAllProductsByName(name);
-    }
-
     // categories
     @GetMapping("/categories/{id}")
     public List<Product> getProductByCategory(@PathVariable("id") int id){
