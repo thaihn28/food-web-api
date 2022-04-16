0a6ff1a0c0fb3504de571d6f376434c6bf49ff7c
thaihn2801 <hoangthai.solem2801@gmail.com>
2022-04-16 15:10:11 +0700

    re-deploy

--- src/main/java/com/example/ecommerceweb/ECommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/ECommerceWebApplication.java
@@ -2,11 +2,26 @@ package com.example.ecommerceweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ECommerceWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommerceWebApplication.class, args);
     }
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/api/**").allowedOrigins("https://sqafood.herokuapp.com");
+                }
+            };
+        }
 
 }
