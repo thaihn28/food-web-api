fcca70563b4173471e924740a4200e649b7a6951
thaihn2801 <hoangthai.solem2801@gmail.com>
2022-04-16 15:23:39 +0700

    re-deploy v2

--- src/main/java/com/example/ecommerceweb/ECommerceWebApplication.java
+++ src/main/java/com/example/ecommerceweb/ECommerceWebApplication.java
@@ -3,25 +3,45 @@ package com.example.ecommerceweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 @SpringBootApplication
+
 public class ECommerceWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommerceWebApplication.class, args);
     }
 
-        @Bean
-        public WebMvcConfigurer corsConfigurer() {
-            return new WebMvcConfigurer() {
-                @Override
-                public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/api/**").allowedOrigins("https://sqafood.herokuapp.com");
-                }
-            };
-        }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/**").allowedOrigins("https://sqafood.herokuapp.com");
+//            }
+//        };
+//    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://sqafood.herokuapp.com")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                ;
+            }
+        };
+    }
 }
+
